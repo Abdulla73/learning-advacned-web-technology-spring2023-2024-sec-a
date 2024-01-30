d68d8d127ec4d6a0a1a9fbe95cbb3205b9aaad53
Abdulla-Bin-Islam <abd679405@gmail.com>
2024-01-30 19:18:36 +0600

    Control flow added

--- Lab Task 1/Control-Flow.js
+++ Lab Task 1/Control-Flow.js
@@ -10,9 +10,52 @@ else{
     console.log("Minor");
 }
 
-let age2 =18;
-let status;
+let age2 =15;
+let statuss;
 
 switch (true){
-    case
+    case age2>=18:
+        statuss="Adult";
+        console.log(statuss);
+        break;
+    case age2>=13:
+        statuss="Teenager";
+        console.log(statuss);
+        break;
+    default:
+        statuss="Minor";
+        console.log(statuss);
+}
+
+let x =["a","b","c","d","e"]
+
+for(i=0; i<3;i++){
+    console.log(x[i]);
+}
+
+let x2 =["a","b","c","d","e"]
+let i=0;
+while(i<4){
+    console.log(x2[i]);
+    i++
+}
+
+let x3 =["a","b","c","d","e"]
+let j=0;
+do{
+    console.log(x3[i]);
+    j++;
+    
+}while(j<4);
+
+let x4 =["a","b","c","d","e"]
+
+for(let i in x4){
+    console.log(x4[i]);
+}
+
+let x5 =["a","b","c","d","e"]
+
+for(let i in x5){
+    console.log(x5[i]);
 }
