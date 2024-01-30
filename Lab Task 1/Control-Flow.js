5000192973dae12aa92e8fd3b6f04d5d79cfedaa
Abdulla-Bin-Islam <abd679405@gmail.com>
2024-01-30 13:59:14 +0600

    lab task 1

---
+++ Lab Task 1/Control-Flow.js
@@ -0,0 +1,18 @@
+let age=18;
+
+if (age >=18){
+    console.log("Adult");
+}
+else if(age<=13){
+    console.log("Tenneger");
+}
+else{
+    console.log("Minor");
+}
+
+let age2 =18;
+let status;
+
+switch (true){
+    case
+}
--- Lab Task 1/function.js
+++ Lab Task 1/function.js
@@ -4,9 +4,31 @@ function greet(name){
 
 greet("Abdullah");
 
+let greet2 = name=> console.log(`'Hello,${name}'`);
+greet2("Abdullah");
+
 function sum(a,b){
     return a+b;
 }
 
 let result =sum (a=5,b=10);
 console.log(result);
+
+
+function operateOneNumber(a,b,operation){
+    return operation(a,b);
+}
+
+function add (a,b){
+    return a+b;
+}
+
+function multiply (a,b){
+    return a*b;
+}
+
+let resultAddition = operateOneNumber(2,5 ,add);
+let resultMulti =operateOneNumber(2,5,multiply);
+
+console.log(resultAddition);
+console.log(resultMulti);
